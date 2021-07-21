0df73211ec75c17bc07e72199e1a5646ba8dd776
Aditi3 <aditi.gvc@gmail.com>
2021-07-21 13:04:39 +0530

    Add Web Service Class

--- CoffeeShop/CoffeeShop/Controllers/OrderTableViewController.swift
+++ CoffeeShop/CoffeeShop/Controllers/OrderTableViewController.swift
@@ -19,3 +19,21 @@ class OrderTableViewController: UIViewController {
     }
     
 }
+
+
+extension OrderTableViewController: UITableViewDataSource {
+   
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "coffeeCell", for: indexPath)
+        return cell
+    }
+    
+    
+}
+extension OrderTableViewController: UITableViewDelegate {
+    
+}
